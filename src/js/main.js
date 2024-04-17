8213f257009fe0c5128fb1878df1553e2bca88f7
carobte <caro.bustamante.escobar@gmail.com>
2024-04-17 10:56:52 -0500

    declaración y operaciones

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,18 @@
-console.log("Números");
+console.log("Numbers")
+
+/* # Creación de números
+
+
+let numero1 = 45
+let numero2 = Number(45)
+let numero3 = new Number(45)
+
+// número legible: 5_000_000 -> 5 millones, no altera significado pero ayuda visualmente al desarrollador
+// Number.MIN_VALUE -> MIN y MAX muestran el rango seguro de JS
+
+// Operaciónes matemáticas */
+
+const numero1 = 5
+const numero2 = "10"
+
+// Para restar, multiplicar y dividir, JS permite hacerlo num con string. Pero si ponemos + lo que hace es concatenar y devuelve un str
